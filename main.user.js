4fc9185dc5ffe20b6cccd6a2dfa2aa62b6ad90f7
Haruo <haruo-mtok@outlook.com>
2024-05-27 17:18:55 +0900

    様々なソート基準に対応

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtCoderCenterMyStanding
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  デフォルトで自身の順位付近を表示するようにします
 // @author       haruomaki
 // @match        https://atcoder.jp/*standings*
@@ -11,11 +11,10 @@
 // ==/UserScript==
 
 // TODO: バーチャル順位表に対応
-// FIXME: ソート基準を順位以外にしたとき
 
 (function () {
     'use strict';
-    /* global vueStandings, vueResults */
+    /* global vueStandings, vueResults, userScreenName */
 
     // Configs
     // ---------------------------
@@ -43,24 +42,17 @@
         return;
     }
 
-    function getRankElement() {
-        if (exists("vueStandings")) {
-            return document.querySelector("#standings-tbody .info td.standings-rank");
-        }
-        if (exists("vueResults")) {
-            return document.querySelector(".info td:nth-child(2)");
-        }
-    }
-
     function getMyStanding() {
         return document.querySelector("tr.info");
     }
 
-    function myRank() {
-        const rank_cell = getRankElement();
-        const str = rank_cell.innerText;
-        const match = str.match(/\d+(?=\D*$)/);
-        return match ? parseInt(match[0], 10) : NaN;
+    function myRank(user_id) {
+        if (exists("vueStandings")) {
+            return vue.orderedStandings.findIndex(data => data.UserScreenName == user_id) + 1;
+        }
+        if (exists("vueResults")) {
+            return vue.orderedResults.findIndex(data => data.UserScreenName == user_id) + 1;
+        }
     }
 
     function clickActivePagination() {
@@ -75,10 +67,12 @@
             clickActivePagination(); // 一度クリックしておかないと、なぜかページ移動が効かない
         }
 
-        const rank = myRank();
+        const user_id = userScreenName;
+        const rank = myRank(user_id); // 存在しなければ0が返る
+        if (rank == 0) return;
 
         const target_page = Math.ceil(rank / vue.perPage);
-        console.debug("center me:", { rank, target_page });
+        console.debug("center me:", { user_id, rank, target_page });
         vue.page = target_page; // これで遷移
     }
 
@@ -89,6 +83,7 @@
             current_row.removeEventListener('click', center_me);
         }
         const row = getMyStanding();
+        if (row == null) return;
         //console.debug("クリック可能にします:", row);
         row.setAttribute('title', 'Click to center my standing');
         row.addEventListener('click', center_me);
@@ -98,9 +93,6 @@
     function main() {
         // 自身の順位をクリックするとページ移動
         remakeClickable();
-
-        // クリック可能なクラスを定義
-        const clickableClassName = 'info';
 
         // 自順位の行が移動もしくは生成されるたびにremake
         const observer = new MutationObserver(mutations => {
